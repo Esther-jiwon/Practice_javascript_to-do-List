c7735635020236b3967b1ba3c7bd21298dbc71c1
Esther-jiwon <estheryun0807@gmail.com>
2021-06-05 15:56:36 +0900

    0.4 Events part Two

--- Login/app.js
+++ Login/app.js
@@ -1,6 +1,7 @@
 const loginForm = document.getElementById("login-form");
 const loginInput  = loginForm.querySelector("input");
 
+const link = document.querySelector("a");
 
 function onLoginSubmit(event) {
     const username = loginInput.value;
@@ -8,5 +9,8 @@ function onLoginSubmit(event) {
     console.log(username);
 }
 
+function handleLinkClick(event) {
+    event.preventDefault();
+}
 loginForm.addEventListener("submit", onLoginSubmit);
-
+link.addEventListener("click", handleLinkClick);
